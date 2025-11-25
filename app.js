def29bb2091130500870093a41b120b1c9a1c4ab
Caroline <carol.carolineleoni@gmail.com>
2025-11-24 21:11:34 -0300

    alterando limite para 100

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let listaDeNumerosSorteados = [];
-let numeroLimite = 10;
+let numeroLimite = 100;
 
 let numeroSecreto = gerarNumeroAleatorio(); //chama a função do número aleatório
 let tentativas = 1;
@@ -12,9 +12,9 @@ function exibirTextoNaTela(tag, texto) { //função com parâmetro. Parâmetros 
 
 function exibirMensagemInicial() {
     exibirTextoNaTela('h1', 'Jogo do número secreto');
-    exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 100');
 
-}
+} 
 
 exibirMensagemInicial();
 
