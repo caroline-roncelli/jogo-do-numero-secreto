85b757a8294cd34efe96c7dc5eb4cdd3bc064564
Caroline <carol.carolineleoni@gmail.com>
2025-12-06 18:15:35 -0300

    alterando limite jogo para 10

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let listaDeNumerosSorteados = [];
-let numeroLimite = 100;
+let numeroLimite = 10;
 
 let numeroSecreto = gerarNumeroAleatorio(); //chama a função do número aleatório
 let tentativas = 1;
@@ -12,7 +12,7 @@ function exibirTextoNaTela(tag, texto) { //função com parâmetro. Parâmetros 
 
 function exibirMensagemInicial() {
     exibirTextoNaTela('h1', 'Jogo do número secreto');
-    exibirTextoNaTela('p', 'Escolha um número entre 1 e 100');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
 
 } 
 
